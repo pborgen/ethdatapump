52e92a6d5d64bd0fdf85760fb8c4feff081adc7a
pborgennc@gmail.com <pborgennc@gmail.com>
2022-02-03 10:07:12 -0500

    update

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ const program = require('commander');
 const ExportTransactions = require('../lib/export_transactions');
 const HexTransactionFinder = require('../lib/HexTransactionFinder');
 const ExportTransactionsFromToAddress = require('../lib/export_transactions_from_to_address');
+const GetTransaction = require('../lib/GetTransaction');
 
 program
     .command('export_transactions') // sub-command name
@@ -26,23 +27,32 @@ program
 
 program
     .command('export_transactions_from_to_address') // sub-command name
-    .alias('e') // alternative sub-command is `al`
+    .alias('eft') 
     .description('export transactions from/to address') // command description
 
     // function to execute when command is uses
     .action(function () {
-        //const start_block = 9041184;
-        const start_block = 9000000;
-        const number_blocks_to_process = 4000000;
-        const end_block = start_block + number_blocks_to_process;
-        const batch_size = 500;
-
+        const base_path = '/home/paul/dev';
+        
+        const block_number_with_transactions_full_path = '';
         let exportTransactionsFromToAddress = new ExportTransactionsFromToAddress();
-        exportTransactionsFromToAddress.export(start_block, end_block);
+        exportTransactionsFromToAddress.export(base_path + '/test.csv');
 
         console.log('Complete bla');
     });
-    
+
+program
+    .command('getTransaction') // sub-command name
+    .alias('et') 
+    .description('getTransaction') // command description
+
+    // function to execute when command is uses
+    .action(function () {
+        let getTransaction = new GetTransaction();
+        getTransaction.export('0xf449665bddefcaf3f2bebe7aa54b93cb5b3a30c3b4c04d498bdcf8b2edca4bdf');
+
+        console.log('Complete bla');
+    });
 
 program
     .command('find_hex_transactions') // sub-command name
