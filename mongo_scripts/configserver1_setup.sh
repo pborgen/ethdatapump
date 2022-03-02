5293d961636141f0fab68508f7ed932783345432
pborgennc@gmail.com <pborgennc@gmail.com>
2022-03-02 12:49:47 -0500

    update

--- mongo_scripts/configserver1_setup.sh
+++ mongo_scripts/configserver1_setup.sh
@@ -1,2 +1,3 @@
-mkdir -p /mnt/crypto/configserver1
-sudo mongod --logpath "/mnt/crypto/configserver1/log.log" --dbpath /mnt/crypto/configserver1 --replSet conf --port 27011  --configsvr
+sudo mkdir -p /mnt/crypto/configserver1
+sudo chmod 777 /mnt/crypto/configserver1
+sudo mongod --logpath "/mnt/crypto/configserver1/log.log" --dbpath /mnt/crypto/configserver1 --replSet conf --port 27011  --configsv &
